4fe4677a4bd236d5395ac43346fd5dc0160e47df
yanjiajun1 <2948979834@qq.com>
2019-10-09 23:35:53 +0800

    3

--- src/main.go
+++ src/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "./fetcher"
+import (
+	"./fetcher"
+	"fmt"
+	"regexp"
+)
 func main() {
-	fetcher.Fetch("http://www.zhenai.com/zhenghun")
-
-
+	all,_:=fetcher.Fetch("http://www.zhenai.com/zhenghun")
+    re:=regexp.MustCompile(`<a href="http://www.zhenai.com/zhenghun/[0-9a-z]+"[^>]*[^<]+</a>`)
+    matches:=re.FindAll(all,-1)
+    for _,m:=range matches{
+    	fmt.Printf("%s\n",m)
+	}
 }
