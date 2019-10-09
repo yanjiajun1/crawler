8c1d61d6e1850888c63c34c520f81a8656977a07
yanjiajun1 <2948979834@qq.com>
2019-10-09 22:59:22 +0800

    "second"

---
+++ src/fetcher/fetcher.go
@@ -0,0 +1,43 @@
+package fetcher
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+func Fetch(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+
+		return nil,
+			fmt.Errorf("wrong status code %d", resp.StatusCode)
+	}
+
+	e := determineEncoding(resp.Body)
+	uf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+	all, err1 := ioutil.ReadAll(uf8Reader)
+
+	if err1 != nil {
+		panic(err1)
+	}
+	fmt.Printf("%s", all)
+	return all, nil
+}
+func determineEncoding(reader io.Reader) encoding.Encoding {
+	bytes, err := bufio.NewReader(reader).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
